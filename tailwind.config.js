041a55ca11a18c1fe377d1c636032ecae68656c8
andresece0709 <149332161+andresece0709@users.noreply.github.com>
2024-02-14 02:59:13 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },darkMode: "class", plugins: [require("tw-elements/dist/plugin.cjs")],
+
+}
+
